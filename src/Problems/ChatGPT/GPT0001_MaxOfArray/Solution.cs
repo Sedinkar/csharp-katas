5a3b6be5680d62ec6a93b9f2b4f5b08f3e4740ba
Sedinkar <iskanderik1000@gmail.com>
2025-08-12 20:07:28 +0500

    GPT0001: solution

--- src/Problems/ChatGPT/GPT0001_MaxOfArray/Solution.cs
+++ src/Problems/ChatGPT/GPT0001_MaxOfArray/Solution.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿namespace Problems.ChatGPT.GPT0001MaxOfArray;
 
-public class Class1
+public static class Solution
 {
-	public Class1()
-	{
-	}
+    /// <summary>
+    /// Возвращает максимальный элемент массива.
+    /// </summary>
+    public static int FindMax(int[] nums)
+    {
+        ArgumentNullException.ThrowIfNull(nums);
+        if (nums.Length == 0) throw new ArgumentException("Array must have at least one element.", nameof(nums));
+
+        var max = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > max) max = nums[i];
+        }
+        return max;
+    }
 }
