140c7b0a9bdf30f3a38f5e3143739b9fdd0ee54b
Sedinkar <iskanderik1000@gmail.com>
2025-08-13 22:00:35 +0500

    LC0009: solution

---
+++ src/Problems/LeetCode/LC0009_Palindromenumber/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace Problems.LeetCode.LC0009.PalindromeNumber;
+
+public class Solution
+{    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        int num = x;
+        int reverseNum = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            reverseNum *= 10;
+            reverseNum += digit;
+            x /= 10;
+        }
+        return reverseNum == num;
+    }
+}
